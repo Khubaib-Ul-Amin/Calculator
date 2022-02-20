f50cc363b40afaa79a7f87f6222b7c50c4220153
khebiii <khubaibulamin88@gmail.com>
2022-02-21 02:24:23 +0500

    finally we add code of every file

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (obj of buttons) {
+   obj.addEventListener('click', (e)=>{
+       buttonText = e.target.innerText;
+       console.log('yes' , buttonText);
+if (buttonText == 'x') {
+   buttonText = '*';
+   screenValue += buttonText;
+   screen.value = screenValue;
+}
+else if (buttonText == 'c') {
+   screenValue = ' ';
+   screen.value = screenValue;
+}
+else if (buttonText == '=') {
+   screen.value = eval(screenValue);
+}
+else {
+   screenValue += buttonText;
+   screen.value = screenValue;
+}
+
+
+
+      
+   })
+}
